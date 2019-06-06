59d51d89640a43428eab132212620311520f0f5f
jfrench <jamesrf999@gmail.com>
2019-06-06 19:11:52 +0100

    Extend tests

--- test/tests/test_key_value_attribute.rb
+++ test/tests/test_key_value_attribute.rb
@@ -34,8 +34,22 @@ class TestKeyValueAttribute < JabaTest
         end
       end
       test :t do
+        # Test basic set
         a :k, :v
         a[:k].must_equal(:v)
+        
+        # Overwrite value
+        a :k, nil
+        a[:k].must_be_nil
+        
+        # Overwrite back to original
+        a :k, :v
+        a[:k].must_equal(:v)
+        
+        # Change key. Old key/value is overwritten
+        a :k2, :v2
+        a[:k2].must_equal(:v2)
+        a[:k].must_be_nil
       end
     end
   end
@@ -45,12 +59,17 @@ class TestKeyValueAttribute < JabaTest
       define :test do
         attr_array :a, type: :keyvalue do
         end
+        attr_array :b, type: :keyvalue do
+          default [{a: :b}, {c: :d}]
+        end
       end
       test :t do
         a :k1, :v1
         a :k2, :v2
         a :k3, :v3
         a.must_equal([{k1: :v1}, {k2: :v2}, {k3: :v3}])
+        # TODO: this is failing
+        #b.must_equal [{a: :b}, {c: :d}]
       end
     end
   end
