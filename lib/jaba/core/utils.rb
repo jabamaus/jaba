2eb450b724841d75c3052b50303a141cdc4cd8fb
jfrench <jamesrf999@gmail.com>
2019-06-26 08:23:24 +0100

    Add generate_guid util

--- lib/jaba/core/utils.rb
+++ lib/jaba/core/utils.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'fiddle'
 require 'tsort'
 
 ##
@@ -20,6 +21,20 @@ module JABA
     #
     def self.mac?
       false
+    end
+    
+    ##
+    #
+    def self.generate_guid
+      if windows?
+        result = ' ' * 16
+        rpcrt4 = Fiddle.dlopen('rpcrt4.dll')
+        uuid_create = Fiddle::Function.new(rpcrt4['UuidCreate'], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_LONG)
+        uuid_create.call(result)
+        sprintf('{%04X%04X-%04X-%04X-%04X-%04X%04X%04X}', *result.unpack('SSSSSSSS')).upcase
+      else
+        raise 'generate_guid not implemented on this platform'
+      end
     end
     
   end
