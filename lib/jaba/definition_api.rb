fa203e20594134f1e9167ffd5a160a5e4387fd98
jfrench <jamesrf999@gmail.com>
2020-04-02 21:11:47 +0100

    Remove

--- lib/jaba/definition_api.rb
+++ lib/jaba/definition_api.rb
@@ -42,14 +42,9 @@ module JABA
     
   end
 
-  require_relative 'extension_api'
-
   ##
-  # API for creating instances of Jaba types.
   #
   class TopLevelAPI < BasicObject
-    
-    include TopLevelExtensionAPI
     
     ##
     # Define a project.
@@ -86,6 +81,35 @@ module JABA
     end
     
     ##
+    # EXTENSION API
+    #
+    def attr_type(id, &block)
+      @obj.define_attr_type(id, &block)
+    end
+    
+    ##
+    # EXTENSION API
+    #
+    def attr_flag(id)
+      @obj.define_attr_flag(id)
+    end
+    
+    ##
+    # EXTENSION API
+    #
+    def define(type, **options, &block)
+      @obj.define_type(type, **options, &block)
+    end
+    
+    ##
+    # EXTENSION API
+    #
+    def open(type, &block)
+      @obj.open_type(type, &block)
+    end
+    
+    ##
+    # Internal use only.
     #
     def method_missing(type, id, **options, &block)
       @obj.define_instance(type, id, **options, &block)
