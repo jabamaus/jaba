a79adf21eb553876296b5d3f0dff7c95f71ac59b
jfrench <jamesrf999@gmail.com>
2020-04-11 09:02:30 +0100

    Further refactoring

--- lib/jaba/core/attribute_definition.rb
+++ lib/jaba/core/attribute_definition.rb
@@ -6,6 +6,39 @@ module JABA
 
   ##
   #
+  class AttributeTypeDefinitionInterface < BasicObject
+
+    include DefinitionCommon
+
+    ##
+    #
+    def init_attr_def(&block)
+      @attr_type_def.define_hook(:init_attr_def, &block)
+    end
+    
+    ##
+    #
+    def validate_attr_def(&block)
+      @attr_type_def.define_hook(:validate_attr_def, &block)
+    end
+    
+    ##
+    #
+    def validate_value(&block)
+      @attr_type_def.define_hook(:validate_value, &block)
+    end
+
+    ##
+    #
+    def initialize(attr_type_def)
+      @attr_type_def = attr_type_def
+      @obj = attr_type_def
+    end
+
+  end
+
+  ##
+  #
   class AttributeTypeDefinition < DefinitionObject
 
     attr_reader :type
@@ -21,32 +54,98 @@ module JABA
       @init_attr_def_hook = nil
       @validate_attr_def_hook = nil
       @validate_value_hook = nil
+      @definition_interface = AttributeTypeDefinitionInterface.new(self)
       eval_definition(&info.block) if info.block
     end
 
     ##
-    # DEFINITION API
     #
-    def init_attr_def(&block)
-      define_hook(:init_attr_def, &block)
+    def eval_obj(context)
+      case context
+      when :definition
+        @definition_interface
+      when :internal
+        self
+      else
+        raise "invalid context #{context}"
+      end
+    end
+
+  end
+  
+  ##
+  #
+  class AttributeDefinitionInterface < BasicObject
+
+    include DefinitionCommon
+
+    ##
+    # Set help for the attribute. Required.
+    #
+    def help(val = nil, &block)
+      @attr_def.set_var(:help, val, &block)
     end
     
     ##
-    # DEFINITION API
+    # Set any number of flags to control the behaviour of the attribute.
     #
-    def validate_attr_def(&block)
-      define_hook(:validate_attr_def, &block)
+    def flags(*flags, &block)
+      @attr_def.set_var(:flags, flags.flatten, &block)
     end
     
     ##
-    # DEFINITION API
+    # Set attribute default value. Can be specified as a value or a block.
     #
-    def validate_value(&block)
-      define_hook(:validate_value, &block)
+    def default(val = nil, &block)
+      @attr_def.set_var(:default, val, &block)
+    end
+    
+    ##
+    #
+    def keyval_options(*options, &block)
+      @attr_def.set_var(:keyval_opts, options.flatten, &block)
+    end
+    
+    ##
+    # Called for single value attributes and each element of array attributes.
+    #
+    def validate(&block)
+      @attr_def.define_hook(:validate, &block)
+    end
+    
+    ##
+    #
+    def post_set(&block)
+      @attr_def.define_hook(:post_set, &block)
+    end
+    
+    ##
+    #
+    def make_handle(&block)
+      @attr_def.define_hook(:make_handle, &block)
+    end
+    
+    ##
+    #
+    def add_property(id, val = nil)
+      @attr_def.set_var(id, val)
+    end
+
+    ##
+    #
+    def method_missing(id, val = nil)
+      @attr_def.handle_property(id, val)
+    end
+
+    ##
+    #
+    def initialize(attr_def)
+      @attr_def = attr_def
+      @obj = attr_def
     end
     
   end
-  
+
   ##
   # Manages shared data that is common to Attributes instanced from this definition.
   #
@@ -70,6 +169,8 @@ module JABA
       @jaba_type = jaba_type
       @api_call_line = api_call_line
 
+      @definition_interface = AttributeDefinitionInterface.new(self)
+
       @default = nil
       @flags = []
       @help = nil
@@ -88,80 +189,23 @@ module JABA
     
     ##
     #
+    def eval_obj(context)
+      case context
+      when :definition
+        @definition_interface
+      when :internal
+        self
+      else
+        raise "invalid context #{context}"
+      end
+    end
+
+    ##
+    #
     def get_default
       @default
     end
     
-    ##
-    # DEFINITION API
-    #
-    # Set help for the attribute. Required.
-    #
-    def help(val = nil, &block)
-      set_var(:help, val, &block)
-    end
-    
-    ##
-    # DEFINITION API
-    #
-    # Set any number of flags to control the behaviour of the attribute.
-    #
-    def flags(*flags, &block)
-      set_var(:flags, flags.flatten, &block)
-    end
-    
-    ##
-    # DEFINITION API
-    #
-    # Set attribute default value. Can be specified as a value or a block.
-    #
-    def default(val = nil, &block)
-      set_var(:default, val, &block)
-    end
-    
-    ##
-    # DEFINITION API
-    #
-    def keyval_options(*options, &block)
-      set_var(:keyval_opts, options.flatten, &block)
-    end
-    
-    ##
-    # DEFINITION API
-    #
-    # Called for single value attributes and each element of array attributes.
-    #
-    def validate(&block)
-      define_hook(:validate, &block)
-    end
-    
-    ##
-    # DEFINITION API
-    #
-    def post_set(&block)
-      define_hook(:post_set, &block)
-    end
-    
-    ##
-    # DEFINITION API
-    #
-    def make_handle(&block)
-      define_hook(:make_handle, &block)
-    end
-    
-    ##
-    # DEFINITION API
-    #
-    def add_property(id, val = nil)
-      set_var(id, val)
-    end
-    
-    ##
-    #
-    def method_missing(id, val = nil)
-      handle_property(id, val)
-    end
-
     ##
     #
     def has_flag?(flag)
