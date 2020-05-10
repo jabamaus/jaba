27d7b09db7befc164690496b6cb0985bbd0ce39a
jabamaus <49597106+jabamaus@users.noreply.github.com>
2020-05-10 07:45:01 +0100

    Comment

--- lib/jaba/core/jaba_attribute.rb
+++ lib/jaba/core/jaba_attribute.rb
@@ -91,7 +91,8 @@ module JABA
     end
 
     ##
-    # Returns the value of the attribute.
+    # Returns the value of the attribute. If value is a reference to a JabaNode and the call to value() came from user
+    # definitions then return the node's attributes rather than the node itself.
     #
     def value(api_call_line = nil)
       if !set?
