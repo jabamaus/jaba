1829634f1ac3ac5d4f020c3def0516fc583c2cec
jabamaus <49597106+jabamaus@users.noreply.github.com>
2020-05-10 21:46:34 +0100

    Move hook methods into their own module

---
+++ lib/jaba/core/hook.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+##
+#
+module JABA
+
+  using JABACoreExt
+
+  ##
+  #
+  module HookMethods
+    
+    ##
+    #
+    def define_hook(id, &block)
+      var = "@#{id}_hook"
+      if instance_variable_defined?(var)
+        jaba_error("'#{id}' hook multiply defined")
+      end
+      if block_given?
+        instance_variable_set(var, block)
+      else
+        instance_variable_set(var, nil)
+      end
+    end
+
+    ##
+    #
+    def set_hook(id, &block)
+      var = "@#{id}_hook"
+      if !instance_variable_defined?(var)
+        jaba_error("'#{id}' hook not defined")
+      end
+      instance_variable_set(var, block)
+    end
+
+    ##
+    #
+    def call_hook(id, args = nil, receiver: self, fail_if_not_set: false)
+      var = "@#{id}_hook"
+      if !instance_variable_defined?(var)
+        jaba_error("'#{id}' hook not defined")
+      end
+      block = instance_variable_get(var)
+      if block.nil?
+        if fail_if_not_set
+          jaba_error("'#{id}' not set - cannot call'")
+        end
+      else
+        receiver.eval_api_block(args, &block)
+      end
+    end
+
+  end
+
+end
--- lib/jaba/core/jaba_object.rb
+++ lib/jaba/core/jaba_object.rb
@@ -10,6 +10,8 @@ module JABA
   #
   class JabaObject
     
+    include HookMethods
+    
     attr_reader :services
     attr_reader :definition
     attr_reader :api
@@ -73,47 +75,6 @@ module JABA
         end
         
         eval_api_block(args, &db.block)
-      end
-    end
-    
-    ##
-    #
-    def define_hook(id, &block)
-      var = "@#{id}_hook"
-      if instance_variable_defined?(var)
-        jaba_error("'#{id}' hook multiply defined")
-      end
-      if block_given?
-        instance_variable_set(var, block)
-      else
-        instance_variable_set(var, nil)
-      end
-    end
-
-    ##
-    #
-    def set_hook(id, &block)
-      var = "@#{id}_hook"
-      if !instance_variable_defined?(var)
-        jaba_error("'#{id}' hook not defined")
-      end
-      instance_variable_set(var, block)
-    end
-
-    ##
-    #
-    def call_hook(id, args = nil, receiver: self, fail_if_not_set: false)
-      var = "@#{id}_hook"
-      if !instance_variable_defined?(var)
-        jaba_error("'#{id}' hook not defined")
-      end
-      block = instance_variable_get(var)
-      if block.nil?
-        if fail_if_not_set
-          jaba_error("'#{id}' not set - cannot call'")
-        end
-      else
-        receiver.eval_api_block(args, &block)
       end
     end
     
