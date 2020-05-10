7b12f1c24dfd55d7d608cc3839a43c9d0204a1be
jabamaus <49597106+jabamaus@users.noreply.github.com>
2020-05-10 05:59:57 +0100

    stop warning

--- test/tests/test_src.rb
+++ test/tests/test_src.rb
@@ -5,14 +5,6 @@ module JABA
   class TestSrc < JabaTest
 
     it 'can be specified explicitly' do
-      r = temp_dir
-      make_file('a.cpp')
-      jaba do
-        #cpp :app do
-        #  root r
-        #  src ['a.cpp']
-        #end
-      end
 
     end
 
