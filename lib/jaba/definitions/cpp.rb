1f338fd1f5b744e81bac8fde6b3e2bd177b1e291
jabamaus <49597106+jabamaus@users.noreply.github.com>
2020-05-20 23:54:48 +0100

    Explicitly define sub types

--- lib/jaba/definitions/cpp.rb
+++ lib/jaba/definitions/cpp.rb
@@ -8,155 +8,161 @@ define :cpp do
     default '.'
   end
 
-  attr :projroot, type: :dir do
-    help 'Directory in which projects will be generated. Specified as a relative path from <root>. If not specified ' \
-     'projects will be generated in <root>'
-    default '.'
-    flags :no_check_exist # May get created during generation
-  end
-  
-  attr :name do
-    help 'Seeds the name of the project. Defaults to the definition id if not set.'
-    default { _ID }
-  end
-  
-  attr :projname do
-    help 'Seeds file basename of project files. Defaults to <name><projsuffix>.'
-    default { "#{name}#{projname_suffix}" }
-  end
-
-  attr :projname_suffix do
-    help 'Optional suffix to be applied to <projname>. Has no effect if <projname> is set explicitly.'
-  end
-
-  attr_array :src, type: :path do
-    help 'Source file specification'
-    flags :unordered # Final source will be sorted so no need to sort this
-    flag_options :export
-  end
-  
   attr_array :platforms, type: :choice do
     items all_instance_ids(:platform)
     flags :required
   end
-  
-  attr :platform, type: :symbol do
-    flags :read_only
-  end
-    
-  attr :platform_ref, type: :reference do
-    referenced_type :platform
-  end
 
-  attr_array :hosts, type: :choice do
-    items all_instance_ids(:host)
-    flags :required
-  end
-  
-  attr :host, type: :symbol do
-    flags :read_only
-  end
+  define :target_platform do
 
-  attr :host_ref, type: :reference do
-    referenced_type :host
-  end
-
-  attr :type, type: :choice do
-    items [:app, :lib, :dll]
-    default :app
-  end
-  
-  attr_array :configs, type: :symbol do
-    flags :required, :unordered
-    flag_options :export
-  end
-  
-  attr_array :deps, type: :reference do
-    referenced_type :cpp
-    make_handle do |id|
-      "cpp|#{id}|#{platform}|#{host}"
-    end
-  end
-  
-  attr_hash :vcglobal do
-    value_option :condition
-    flag_options :export
-  end
-  
-  attr :winsdkver, type: :choice do
-    items ['10.0.16299.0', '10.0.17763.0']
-    default '10.0.17763.0'
-  end
-  
-  define :cpp_config do
-
-    attr_array :build_action, type: :string do
-      help 'Build actions'
-      flag_options :export
-      value_option :msg
-      value_option :type, required: true, items: [:PreBuild, :PreLink, :PostBuild]
-    end
-
-    attr_array :cflags do
-      help 'Compiler command line arguments'
-      flag_options :export
-    end
-
-    attr :config, type: :symbol do
+    attr :platform, type: :symbol do
       flags :read_only
     end
-
-    attr :config_name do
-      help 'Display name of config in Visual Studio. Defaults to <config>.'
-      default { config }
+      
+    attr :platform_ref, type: :reference do
+      referenced_type :platform
+    end
+  
+    attr_array :hosts, type: :choice do
+      items all_instance_ids(:host)
+      flags :required
     end
 
-    attr :debug, type: :bool do
-      help 'Flags config as a debug build. Defaults to true if config id contains \'debug\''
-      default do
-        config =~ /debug/i ? true : false
+    define :project do
+      
+      attr :projroot, type: :dir do
+        help 'Directory in which projects will be generated. Specified as a relative path from <root>. If not specified ' \
+        'projects will be generated in <root>'
+        default '.'
+        flags :no_check_exist # May get created during generation
       end
-    end
+      
+      attr :name do
+        help 'Seeds the name of the project. Defaults to the definition id if not set.'
+        default { _ID }
+      end
+      
+      attr :projname do
+        help 'Seeds file basename of project files. Defaults to <name><projsuffix>.'
+        default { "#{name}#{projname_suffix}" }
+      end
 
-    attr_array :defines, type: :string do
-      help 'Preprocessor defines'
-      flag_options :export
-    end
+      attr :projname_suffix do
+        help 'Optional suffix to be applied to <projname>. Has no effect if <projname> is set explicitly.'
+      end
 
-    attr :exceptions, type: :choice do
-      help 'Enables C++ exceptions. On by default.'
-      items [true, false]
-      items [:structured] # Windows only
-      default true
-    end
+      attr_array :src, type: :path do
+        help 'Source file specification'
+        flags :unordered # Final source will be sorted so no need to sort this
+        flag_options :export
+      end
+      
+      attr :host, type: :symbol do
+        flags :read_only
+      end
 
-    attr_array :inc, type: :dir do
-      help 'Include paths'
-      flags :unordered
-      flag_options :export
-    end
+      attr :host_ref, type: :reference do
+        referenced_type :host
+      end
 
-    attr :rtti, type: :bool do
-      default true
-    end
+      attr :type, type: :choice do
+        items [:app, :lib, :dll]
+        default :app
+      end
+      
+      attr_array :configs, type: :symbol do
+        flags :required, :unordered
+        flag_options :export
+      end
+      
+      attr_array :deps, type: :reference do
+        referenced_type :cpp
+        make_handle do |id|
+          "cpp|#{id}|#{platform}|#{host}"
+        end
+      end
+      
+      attr_hash :vcglobal do
+        value_option :condition
+        flag_options :export
+      end
+      
+      attr :winsdkver, type: :choice do
+        items ['10.0.16299.0', '10.0.17763.0']
+        default '10.0.17763.0'
+      end
+      
+      define :config do
 
-    attr :toolset, type: :string do
-      default { host_ref.toolset }
-    end
+        attr_array :build_action, type: :string do
+          help 'Build actions'
+          flag_options :export
+          value_option :msg
+          value_option :type, required: true, items: [:PreBuild, :PreLink, :PostBuild]
+        end
 
-    attr :unicode, type: :bool do
-      help 'Enables unicode. On by default.'
-      default true
-    end
+        attr_array :cflags do
+          help 'Compiler command line arguments'
+          flag_options :export
+        end
 
-    attr_hash :vcproperty do
-      value_option :group, required: true
-      value_option :condition
-      flag_options :export
-    end
+        attr :config, type: :symbol do
+          flags :read_only
+        end
 
-    attr :warnerror, type: :bool do
-      help 'Enable warnings as errors. Off by default.'
+        attr :config_name do
+          help 'Display name of config in Visual Studio. Defaults to <config>.'
+          default { config }
+        end
+
+        attr :debug, type: :bool do
+          help 'Flags config as a debug build. Defaults to true if config id contains \'debug\''
+          default do
+            config =~ /debug/i ? true : false
+          end
+        end
+
+        attr_array :defines, type: :string do
+          help 'Preprocessor defines'
+          flag_options :export
+        end
+
+        attr :exceptions, type: :choice do
+          help 'Enables C++ exceptions. On by default.'
+          items [true, false]
+          items [:structured] # Windows only
+          default true
+        end
+
+        attr_array :inc, type: :dir do
+          help 'Include paths'
+          flags :unordered
+          flag_options :export
+        end
+
+        attr :rtti, type: :bool do
+          default true
+        end
+
+        attr :toolset, type: :string do
+          default { host_ref.toolset }
+        end
+
+        attr :unicode, type: :bool do
+          help 'Enables unicode. On by default.'
+          default true
+        end
+
+        attr_hash :vcproperty do
+          value_option :group, required: true
+          value_option :condition
+          flag_options :export
+        end
+
+        attr :warnerror, type: :bool do
+          help 'Enable warnings as errors. Off by default.'
+        end
+      end
     end
   end
 
--- lib/jaba/generators/cpp_generator.rb
+++ lib/jaba/generators/cpp_generator.rb
@@ -16,31 +16,22 @@ module JABA
       @project_nodes = []
       @projects = []
     end
-    
-    ##
-    #
-    def sub_type(attr_id)
-      case attr_id
-      when :root, :platforms
-        :cpp_root
-      when :platform, :platform_ref, :hosts
-        :cpp_hosts
-      end
-    end
 
     ##
     #
     def make_nodes
-      root_node = make_node(type_id: :cpp_root)
+      root_node = make_node(type_id: :cpp)
       
+      # TODO: namespacing of sub type ids
+
       root_node.attrs.platforms.each do |p|
-        hosts_node = make_node(type_id: :cpp_hosts, name: p, parent: root_node) do
+        hosts_node = make_node(type_id: :target_platform, name: p, parent: root_node) do
           platform p
           platform_ref p
         end
         
         hosts_node.attrs.hosts.each do |h|
-          proj_node = make_node(type_id: :cpp, name: h, parent: hosts_node) do
+          proj_node = make_node(type_id: :project, name: h, parent: hosts_node) do
             host h
             host_ref h
           end
@@ -48,7 +39,7 @@ module JABA
           @project_nodes << proj_node
           
           proj_node.attrs.configs.each do |cfg|
-            make_node(type_id: :cpp_config, name: cfg, parent: proj_node) do
+            make_node(type_id: :config, name: cfg, parent: proj_node) do
               config cfg
             end
           end
