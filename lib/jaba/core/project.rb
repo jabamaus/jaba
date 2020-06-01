d99a907c475dcf710c78eb89bb55523dca88cf70
jabamaus <49597106+jabamaus@users.noreply.github.com>
2020-06-01 07:04:35 +0100

    Improve code

--- lib/jaba/core/project.rb
+++ lib/jaba/core/project.rb
@@ -56,13 +56,12 @@ module JABA
 
       file_manager = @services.file_manager
       dest = instance_variable_set("@#{src_attr_id}", [])
-      spec_files = []
 
       src_attr.visit_attr do |elem, spec|
-        spec_files.clear
         force = elem.has_flag_option?(:force)
         wildcard = spec =~ /\*/ ? true : false
         abs_spec = !spec.absolute_path? ? "#{@root}/#{spec}" : spec
+        glob_matches = nil
 
         if wildcard
           if force
@@ -70,30 +69,28 @@ module JABA
               'only explicitly specified source files', callstack: src_attr.last_call_location)
           end
           glob_matches = file_manager.glob(abs_spec)
-          if glob_matches.empty?
-            @services.jaba_warning("'#{spec}' did not match any #{src_attr_id} files ", callstack: src_attr.last_call_location)
-          else
-            glob_matches.select!{|f| extensions.include?(f.extname)}
-            spec_files.concat(glob_matches)
-          end
         else # else its an explicitly specified file or directory
           if !file_manager.exist?(abs_spec) && !force
             @services.jaba_error("'#{spec}' does not exist on disk. Use :force to add anyway.", callstack: src_attr.last_call_location)
           end
+
+          # If its a directory add files recursively, else add single file
+          #          
           if File.directory?(abs_spec)
             glob_matches = file_manager.glob("#{abs_spec}/**/*")
-            if glob_matches.empty?
-              @services.jaba_warning("'#{spec}' did not match any #{src_attr_id} files ", callstack: src_attr.last_call_location)
-            else
-              glob_matches.select!{|f| extensions.include?(f.extname)}
-              spec_files.concat(glob_matches)
-            end
           else
-            spec_files << abs_spec
+            dest << abs_spec
           end
         end
 
-        dest.concat(spec_files)
+        if glob_matches
+          if glob_matches.empty?
+            @services.jaba_warning("'#{spec}' did not match any #{src_attr_id} files ", callstack: src_attr.last_call_location)
+          else
+            glob_matches.select!{|f| extensions.include?(f.extname)}
+            dest.concat(glob_matches)
+          end
+        end
       end
 
       if dest.empty?
