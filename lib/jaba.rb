031908fcb4546ffafa9e8a51415ae26c3d7f56fe
jabamaus <49597106+jabamaus@users.noreply.github.com>
2020-06-10 18:37:47 +0100

    More renaming

--- lib/jaba.rb
+++ lib/jaba.rb
@@ -1,11 +1,11 @@
 require 'jaba/core/services'
-require 'jaba/api/api_common'
-require 'jaba/api/jaba_attribute_type_api'
-require 'jaba/api/jaba_attribute_flag_api'
-require 'jaba/api/jaba_attribute_definition_api'
-require 'jaba/api/jaba_node_api'
-require 'jaba/api/jaba_top_level_api'
-require 'jaba/api/jaba_type_api'
+require 'jaba/jdl_api/api_common'
+require 'jaba/jdl_api/jaba_attribute_type_api'
+require 'jaba/jdl_api/jaba_attribute_flag_api'
+require 'jaba/jdl_api/jaba_attribute_definition_api'
+require 'jaba/jdl_api/jaba_node_api'
+require 'jaba/jdl_api/jaba_top_level_api'
+require 'jaba/jdl_api/jaba_type_api'
 
 module JABA
 
