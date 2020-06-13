a5e160f3bcd78275a42d28e8f22fcbdbc34920f0
jabamaus <49597106+jabamaus@users.noreply.github.com>
2020-06-13 21:31:53 +0100

    Move vc globals into better place

---
+++ lib/jaba/modules/cpp/VisualStudio/vcglobals.jdl.rb
@@ -0,0 +1,19 @@
+open_type :globals do
+  attr_hash :vcfiletype, type: :symbol do
+    flags :required
+  end
+end
+
+open_instance :globals, type: :globals do
+  vcfiletype '.h', :ClInclude
+  vcfiletype '.inl', :ClInclude
+  vcfiletype '.hpp', :ClInclude
+  vcfiletype '.cpp', :ClCompile
+  vcfiletype '.c', :ClCompile
+  vcfiletype '.cxx', :ClCompile
+  vcfiletype '.cc', :ClCompile
+  vcfiletype '.png', :Image
+  vcfiletype '.asm', :MASM
+  vcfiletype '.rc', :ResourceCompile
+  vcfiletype '.natvis', :Natvis
+end
