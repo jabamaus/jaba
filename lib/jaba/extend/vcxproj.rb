6baf2071edfaf3260e134b70aba90ee83e78a8b3
jabamaus <49597106+jabamaus@users.noreply.github.com>
2020-06-28 21:56:27 +0100

    Changes to build_action. Issues not yet resolved.

--- lib/jaba/extend/vcxproj.rb
+++ lib/jaba/extend/vcxproj.rb
@@ -36,17 +36,23 @@ module JABA
 
         # Build events. Standard across platforms.
         #
+        action_cmds = {}
         cfg.visit_attr(:build_action) do |a, value|
-          cmd = String.new
           msg = a.get_option_value(:msg, fail_if_not_found: false)
-          cmd << "#{msg}\n" if msg
-          cmd << value
           type = a.get_option_value(:type)
           group = case type
           when :PreBuild, :PreLink, :PostBuild
             "#{type}Event"
           end
-          vcproperty :Command, cmd, group: group
+          if msg
+            action_cmds.push_value(group, "echo #{msg}")
+          end
+          action_cmds.push_value(group, value)
+        end
+
+        action_cmds.each do |group, cmds|
+          puts "#{cmds} group:#{group}"
+          cfg.attrs.vcproperty :Command, cmds.join("\n"), group: group
         end
       end
     end
@@ -89,6 +95,7 @@ module JABA
         cfg = {}
         attrs = c.attrs
         cfg_root[attrs.config] = cfg
+        # TODO: organise by group. Build at the same time is generating
         cfg[:arch] = attrs.arch_ref.defn_id
         cfg[:name] = attrs.configname
         cfg[:defines] = attrs.defines
