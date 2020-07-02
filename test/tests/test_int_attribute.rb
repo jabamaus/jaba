115154da6bf732afe07156b21e6b37e2e22df57e
jabamaus <49597106+jabamaus@users.noreply.github.com>
2020-07-02 13:25:30 +0100

    Improve tests

--- test/tests/test_int_attribute.rb
+++ test/tests/test_int_attribute.rb
@@ -10,7 +10,7 @@ module JABA
         check_fail ':int attributes only accept integer values', line: [__FILE__, 'tagP'] do
           jaba(barebones: true) do
             define :test do
-              attr :b, type: :int do
+              attr :a, type: :int do
                 default 'not an int' # tagP
               end
             end
@@ -22,42 +22,71 @@ module JABA
         check_fail ':int attributes only accept integer values', line: [__FILE__, 'tagW'] do
           jaba(barebones: true) do
             define :test do
-              attr :c, type: :int
+              attr :a, type: :int
             end
-            test :b do
-              c true # tagW
+            test :t do
+              a true # tagW
             end
+          end
+        end
+      end
+
+      it 'fails if value not supplied when :required flag specified' do
+        check_fail "'t.a' attribute requires a value", line: [__FILE__, 'tagY'] do
+          jaba(barebones: true) do
+            define :test do
+              attr :a, type: :bool do
+                flags :required
+              end
+            end
+            test :t # tagY
           end
         end
       end
 
     end
 
-    it 'works with default values' do
+    it 'supports standard ops' do
       jaba(barebones: true) do
         define :test do
           attr :a, type: :int
           attr :b, type: :int do
             default 1
           end
+          attr_array :aa, type: :int
+          attr_array :ba, type: :int do
+            default [3, 4, 5]
+          end
+          attr_hash :ah, type: :int, key_type: :symbol
+          attr_hash :bh, type: :int, key_type: :symbol do
+            default({k1: 1})
+          end
         end
         test :t do
           a.must_equal(0) # Defaults to 0 if :required flag not used
-          b.must_equal(1)
-        end
-      end
-    end
-    
-    it 'works with :required flag' do
-      check_fail "'t.a' attribute requires a value", line: [__FILE__, 'tagY'] do
-        jaba(barebones: true) do
-          define :test do
-            attr :a, type: :bool do
-              flags :required
-            end
-          end
-          test :t do # tagY
-          end
+          b.must_equal(1) # Works with a default
+          a 2
+          a.must_equal(2)
+          b 3
+          b.must_equal(3)
+
+          # test array attrs
+          aa.must_equal []
+          aa [1, 2, 3]
+          aa.must_equal [1, 2, 3]
+          ba.must_equal [3, 4, 5]
+          ba [6, 7, 8]
+          ba.must_equal [3, 4, 5, 6, 7, 8]
+
+          # test hash attrs
+          ah.must_equal({})
+          ah :k1, 1
+          ah.must_equal({k1: 1})
+          bh.must_equal({k1: 1})
+          bh :k1, 2
+          bh.must_equal({k1: 2})
+          bh :k2, 3
+          bh.must_equal({k1: 2, k2: 3})
         end
       end
     end
