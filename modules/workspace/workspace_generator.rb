da33d5ee3842698e0e2696a9eb5ccb4343e1de2d
jabamaus <49597106+jabamaus@users.noreply.github.com>
2020-07-27 08:22:29 +0100

    Tidy up

--- modules/workspace/workspace_generator.rb
+++ modules/workspace/workspace_generator.rb
@@ -15,63 +15,32 @@ module JABA
     ##
     #
     def init
-      @workspace_nodes = []
     end
 
     ##
     #
     def make_nodes
-      root_node = make_node
-      @workspace_nodes << root_node
-
-      @root = make_node_paths_absolute(root_node)
-
-      # TODO: pass this in
-      cpp_gen = get_generator(:cpp)
-      
-      @candidate_projects = cpp_gen.get_projects
-      @projects = []
-
-      projects_attr = root_node.get_attr(:projects)
-      project_specs = projects_attr.value
-      get_matching_projects(project_specs, errline: projects_attr.last_call_location)
-
-      if @projects.empty?
-        jaba_error("No projects matched specs", errline: projects_attr.last_call_location)
-      end
-
-      @configs = {}
-      @projects.each do |p|
-        p.each_config do |cfg|
-          cfg_id = cfg.handle[/^.*?\|(.*)/, 1] # TODO: nasty
-          if !@configs.key?(cfg_id)
-            @configs[cfg_id] = [cfg.attrs.configname, cfg.attrs.arch_ref.attrs.vsname]
-          end
-        end
-      end
-      @configs = @configs.values
-
-      root_node
+      make_node
     end
     
     ##
     # For use by workspace generator. spec is either a defn_id or a wildcard match against projdir.
     # 
-    def get_matching_projects(specs, errline:)
+    def get_matching_projects(projects, root, specs, errline:)
       specs.each do |spec|
         if spec.string? && spec.wildcard?
-          abs_spec = "#{@root}/#{spec}"
+          abs_spec = "#{root}/#{spec}"
           matches = @candidate_projects.select{|p| File.fnmatch?(abs_spec, p.root)}
           if matches.empty?
             jaba_warning("No projects matching spec '#{spec.inspect_unquoted}' found", errline: errline)
           end
-          @projects.concat(matches)
+          projects.concat(matches)
         else # its an id
           matches = @candidate_projects.select{|p| p.handle.start_with?("#{spec}|")}
           if matches.empty?
             jaba_error("No projects matching spec '#{spec.inspect_unquoted}' found", errline: errline)
           end
-          @projects.concat(matches)
+          projects.concat(matches)
         end
       end
     end
@@ -79,9 +48,35 @@ module JABA
     ##
     #
     def make_host_objects
-      @workspace_nodes.each do |wsn|
+      # TODO: pass this in
+      cpp_gen = get_generator(:cpp)
+      @candidate_projects = cpp_gen.get_projects
+
+      @root_nodes.each do |wsn|
         #klass = wsn.attrs.host_ref.attrs.workspace_classname
-        make_workspace('Sln', wsn, @projects, @configs)
+
+        root = make_node_paths_absolute(wsn)
+
+        projects = []
+        projects_attr = wsn.get_attr(:projects)
+        project_specs = projects_attr.value
+        get_matching_projects(projects, root, project_specs, errline: projects_attr.last_call_location)
+  
+        if projects.empty?
+          jaba_error("No projects matched specs", errline: projects_attr.last_call_location)
+        end
+  
+        configs = {}
+        projects.each do |p|
+          p.each_config do |cfg|
+            cfg_id = cfg.handle[/^.*?\|(.*)/, 1] # TODO: nasty
+            if !configs.key?(cfg_id)
+              configs[cfg_id] = [cfg.attrs.configname, cfg.attrs.arch_ref.attrs.vsname]
+            end
+          end
+        end
+        configs = configs.values
+        make_workspace('Sln', wsn, projects, configs)
       end
     end
 
@@ -94,7 +89,7 @@ module JABA
     ##
     # 
     def build_jaba_output(g_root, out_dir)
-      @workspace_nodes.each do |w|
+      @root_nodes.each do |w|
         w_root = {}
         g_root[w.handle] = w_root
         #p.build_jaba_output(p_root, out_dir)
