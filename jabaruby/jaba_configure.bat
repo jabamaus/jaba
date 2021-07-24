4196d2b9faaf9d2c4eaf7be64475e20b8ff274dc
jabamaus <49597106+jabamaus@users.noreply.github.com>
2021-07-24 08:54:05 +0100

    update for ruby 3.0

--- jabaruby/jaba_configure.bat
+++ jabaruby/jaba_configure.bat
@@ -1,3 +1,3 @@
 pushd ..\..\ruby
-win32\configure.bat --with-static-linked-ext --disable-install-doc  --prefix=C:\GitHub\Projects\JabaRuby --with-baseruby=C:\Ruby27-x64\bin\ruby.exe --without-ext="bigdecimal,cgi,continuation,coverage,date,dbm,etc,fcntl,fiber,fiddle,gdbm,io,monitor,nkf,objspace,openssl,pathname,psych,pty,racc,readline,ripper,rubyvm,sdbm,socket,stringio,strscan,syslog,-test-,win32,win32ole,zlib"
+win32\configure.bat --with-static-linked-ext --disable-install-doc  --prefix=C:\GitHub\Projects\JabaRuby --with-baseruby=C:\Ruby30-x64\bin\ruby.exe --without-ext="bigdecimal,cgi,continuation,coverage,date,dbm,etc,fcntl,fiber,fiddle,gdbm,io,monitor,nkf,objspace,openssl,pathname,psych,pty,racc,readline,ripper,rubyvm,socket,stringio,strscan,syslog,-test-,win32,win32ole,zlib"
 popd
