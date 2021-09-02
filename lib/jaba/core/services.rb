190a2f32d458b3fe255247ec9894f8e08db8120d
jabamaus <49597106+jabamaus@users.noreply.github.com>
2021-09-02 17:41:25 +0100

    Revert "Setup access to globals node as early as possible"
    This reverts commit 5b0eafdece980988bbdca17b54b995ae0d9800a8.

--- lib/jaba/core/services.rb
+++ lib/jaba/core/services.rb
@@ -800,10 +800,6 @@ module JABA
         JABA.error("Duplicate node handle '#{handle}'")
       end
       @node_lookup[handle] = node
-      if node.jaba_type.defn_id == :globals
-        @globals_node = node
-        @globals = node.attrs
-      end
     end
 
     ##
