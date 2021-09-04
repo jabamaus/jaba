9e1f34ea46bf730ffa09882d9d83b02647da1fd0
jabamaus <49597106+jabamaus@users.noreply.github.com>
2021-09-04 18:07:04 +0100

    formatting

--- lib/jaba/core/jaba_attribute_type.rb
+++ lib/jaba/core/jaba_attribute_type.rb
@@ -82,15 +82,11 @@ module JABA
   #
   class JabaAttributeTypeString < JabaAttributeType
     
-    ##
-    #
     def initialize
       super(:string, 'String attribute type', default: '')
       @notes = 'Only explicit strings will be accepted. Symbols are not valid. Defaults to empty string unless value must be specified by user.'
     end
 
-    ##
-    #
     def validate_value(attr_def, value)
       if !value.string?
         JABA.error("'#{value}' must be a string but was a '#{value.class}'")
@@ -103,21 +99,15 @@ module JABA
   #
   class JabaAttributeTypeSymbol < JabaAttributeType
     
-    ##
-    #
     def initialize
       super(:symbol, 'Symbol attribute type')
       @notes = 'Only explicit symbols will be accepted. Strings are not valid.'
     end
 
-    ##
-    #
     def from_cmdline(str, attr_def)
       str.to_sym
     end
 
-    ##
-    #
     def validate_value(attr_def, value)
       if !value.symbol?
         JABA.error("'#{value}' must be a symbol but was a '#{value.class}'")
@@ -130,15 +120,11 @@ module JABA
   #
   class JabaAttributeTypeSymbolOrString < JabaAttributeType
     
-    ##
-    #
     def initialize
       super(:symbol_or_string, 'Symbol or string attribute type')
       @notes = 'Only explicit strings or symbols will be accepted'
     end
 
-    ##
-    #
     def validate_value(attr_def, value)
       if !value.symbol? && !value.string?
         JABA.error("'#{value}' must be a symbol or a string but was a '#{value.class}'")
@@ -147,20 +133,16 @@ module JABA
     
   end
 
-    ##
+  ##
   #
   class JabaAttributeTypeToS < JabaAttributeType
     
-    ##
-    #
     def initialize
       super(:to_s, 'to_s attribute type')
       @notes = 'Any object that supports that can be converted to a string with to_s will be accepted. This is very permissive as ' \
       'in practice this is just about anything in ruby - this type is here to make that intention explcit.'
     end
 
-    ##
-    #
     def validate_value(attr_def, value)
       if !value.respond_to?(:to_s)
         JABA.error("'#{value}' must respond to 'to_s' method but '#{value.class}' did not")
@@ -173,15 +155,11 @@ module JABA
   #
   class JabaAttributeTypeInt < JabaAttributeType
   
-    ##
-    #
     def initialize
       super(:int, 'Integer attribute type', default: 0)
       @notes = 'Defaults to 0 unless value must be specified by user'
     end
 
-    ##
-    #
     def from_cmdline(str, attr_def)
       begin
         Integer(str)
@@ -190,8 +168,6 @@ module JABA
       end
     end
 
-    ##
-    #
     def validate_value(attr_def, value)
       if !value.integer?
         JABA.error(":int attributes only accept integer values but got [value=#{value}, class=#{value.class}]")
@@ -204,15 +180,11 @@ module JABA
   #
   class JabaAttributeTypeBool < JabaAttributeType
     
-    ##
-    #
     def initialize
       super(:bool, 'Boolean attribute type', default: false)
       @notes = 'Accepts [true|false]. Defaults to false unless value must be supplied by user.'
     end
 
-    ##
-    #
     def from_cmdline(str, attr_def)
       case str
       when 'true', '1'
@@ -224,16 +196,12 @@ module JABA
       end
     end
 
-    ##
-    #
     def post_init_attr_def(attr_def)
       if attr_def.array?
         attr_def.set_property(:flags, [:no_sort, :allow_dupes])
       end
     end
 
-    ##
-    #
     def validate_value(attr_def, value)
       if !value.boolean?
         JABA.error(":bool attributes only accept [true|false] but got '#{value}'")
@@ -246,15 +214,11 @@ module JABA
   #
   class JabaAttributeTypeChoice < JabaAttributeType
     
-    ##
-    #
     def initialize
       super(:choice, 'Choice attribute type')
       @notes = 'Can take exactly one of a set of unique values'
     end
 
-    ##
-    #
     def from_cmdline(str, attr_def)
       items = attr_def.items
       
@@ -267,20 +231,14 @@ module JABA
       items[index]
     end
 
-    ##
-    #
     def get_reference_manual_rows(attr_def)
       { choices: attr_def.items.inspect }
     end
     
-    ##
-    #
     def init_attr_def(attr_def)
       attr_def.define_array_property(:items)
     end
 
-    ##
-    #
     def post_init_attr_def(attr_def)
       items = attr_def.items
       if items.empty?
@@ -290,8 +248,6 @@ module JABA
       end
     end
 
-    ##
-    #
     def validate_value(attr_def, value)
       items = attr_def.items
       if !items.include?(value)
@@ -317,14 +273,10 @@ module JABA
       :jaba_file          # path will be based on the directory of the jaba definition file the path was set in
     ]
     
-    ##
-    #
     def init_attr_def(attr_def)
       attr_def.define_property(:basedir_spec)
     end
 
-    ##
-    #
     def post_init_attr_def(attr_def)
       if !VALID_BASE_SPECS.include?(attr_def.basedir_spec)
         JABA.error("'basedir_spec' must be one of #{VALID_BASE_SPECS}")
@@ -337,15 +289,11 @@ module JABA
   #
   class JabaAttributeTypeFile < PathAttrBase
     
-    ##
-    #
     def initialize
       super(:file, 'File attribute type')
       @notes = 'Validates that value is a string path representing a file'
     end
 
-    ##
-    #
     def validate_value(attr_def, file)
       file.validate_path do |msg|
         services.jaba_warn("File '#{file}' not specified cleanly: #{msg}")
@@ -358,15 +306,11 @@ module JABA
   #
   class JabaAttributeTypeDir < PathAttrBase
     
-    ##
-    #
     def initialize
       super(:dir, 'Directory attribute type', default: '.')
       @notes = 'Validates that value is a string path representing a directory'
     end
 
-    ##
-    #
     def validate_value(attr_def, dir)
       dir.validate_path do |msg|
         services.jaba_warn("Directory '#{dir}' not specified cleanly: #{msg}")
@@ -379,15 +323,11 @@ module JABA
   #
   class JabaAttributeTypeSrcSpec < PathAttrBase
     
-    ##
-    #
     def initialize
       super(:src_spec, 'Source file specification pattern')
       @notes = 'Can be file glob match an explicit path or a directory'
     end
 
-    ##
-    #
     def validate_value(attr_def, src_spec)
       src_spec.validate_path do |msg|
         services.jaba_warn("Src spec '#{src_spec}' not specified cleanly: #{msg}")
@@ -400,15 +340,11 @@ module JABA
   #
   class JabaAttributeTypeBasename < JabaAttributeType
   
-    ##
-    #
     def initialize
       super(:basename, 'basename attribute type')
       @notes = 'Basename of a file. Slashes are rejected.'
     end
 
-    ##
-    #
     def validate_value(attr_def, value)
       if value.contains_slashes?
         JABA.error("'#{value}' must not contain slashes")
@@ -421,14 +357,10 @@ module JABA
   #
   class JabaAttributeTypeUUID < JabaAttributeType
     
-    ##
-    #
     def initialize
       super(:uuid, 'UUID attribute type')
     end
 
-    ##
-    #
     def map_value(value)
       JABA.generate_guid(namespace: 'JabaAttributeTypeUUID', name: value)
     end
@@ -445,23 +377,17 @@ module JABA
       super(:node_ref, 'Node reference attribute type')
     end
 
-    ##
-    #
     def get_reference_manual_rows(attr_def)
       ref_type = services.get_jaba_type(attr_def.node_type)
       { references: "[#{attr_def.node_type}](#{ref_type.reference_manual_page})" }
     end
 
-    ##
-    #
     def init_attr_def(attr_def)
       attr_def.define_property(:node_type)
       attr_def.define_block_property(:make_handle)
       attr_def.define_block_property(:unresolved_msg)
     end
 
-    ##
-    #
     def post_init_attr_def(attr_def)
       t = attr_def.node_type
       if t.nil?
@@ -478,26 +404,18 @@ module JABA
   #
   class JabaAttributeTypeNode < JabaAttributeType
     
-    ##
-    #
     def initialize
       super(:node, 'Node attribute type')
     end
 
-    ##
-    #
     def get_reference_manual_rows(attr_def)
       { node_type: attr_def.node_type.inspect }
     end
 
-    ##
-    #
     def init_attr_def(attr_def)
       attr_def.define_property(:node_type)
     end
 
-    ##
-    #
     def post_init_attr_def(attr_def)
       t = attr_def.node_type
       if t.nil?
@@ -512,32 +430,22 @@ module JABA
 
   end
 
-    ##
-  #
   class JabaAttributeTypeBlock < JabaAttributeType
     
-    ##
-    #
     def initialize
       super(:block, 'Block attribute type')
     end
 
-    ##
-    #
     def from_cmdline(str, attr_def)
       JABA.error("block attributes cannot be specified on command line")
     end
 
-    ##
-    #
     def post_init_attr_def(attr_def)
       if attr_def.array?
         attr_def.set_property(:flags, [:no_sort, :allow_dupes])
       end
     end
 
-    ##
-    #
     def validate_value(attr_def, value)
       JABA.error("must be a block") if !value.proc?
     end
