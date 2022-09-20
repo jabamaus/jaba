3806c226b3ff20a5fa2d4e2cb73534c259c9eb8c
jabamaus <49597106+jabamaus@users.noreply.github.com>
2022-09-20 23:34:13 +0100

    cosmetic

--- src/core/standard_paths.rb
+++ src/core/standard_paths.rb
@@ -1,33 +1,13 @@
 module JABA
 
-  ##
-  #
-  def self.jaba_docs_url
-    'https://jabamaus.github.io/jaba'
-  end
-  
-  ##
-  #
+  def self.jaba_docs_url = 'https://jabamaus.github.io/jaba'
+
   def self.install_dir
     @@jaba_install_dir ||= "#{__dir__}/../..".cleanpath
   end
 
-  ##
-  #
-  def self.modules_dir
-    "#{install_dir}/src/modules"
-  end
-
-  ##
-  #
-  def self.grab_bag_dir
-    "#{install_dir}/src/grab_bag"
-  end
-
-  ##
-  #
-  def self.examples_dir
-    "#{install_dir}/examples"
-  end
+  def self.modules_dir = "#{install_dir}/src/modules"
+  def self.grab_bag_dir = "#{install_dir}/src/grab_bag"
+  def self.examples_dir = "#{install_dir}/examples"
 
 end
