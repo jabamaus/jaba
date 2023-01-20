c00f6f1376d49b19deb2e85ae7f6b04a0d515e06
jabamaus <49597106+jabamaus@users.noreply.github.com>
2023-01-20 07:54:18 +0000

    Use vs2022

--- examples/gen_all.rb
+++ examples/gen_all.rb
@@ -10,7 +10,7 @@ if __FILE__ == $PROGRAM_NAME
   iterate_examples do |dir|
     Dir.chdir(dir) do
       puts "Running jaba in #{dir}..."
-      cmdline = 'jaba -D target_host vs2019'
+      cmdline = 'jaba -D target_host vs2022'
       case dir
       when /parameterisable/
         cmdline << " -D cpp_default_lib_type lib"# -D buildsystem_root buildsystem_dynamic"
