26eaa686cafc189d31c8e921c1ce1752162ce2d1
jabamaus <49597106+jabamaus@users.noreply.github.com>
2023-02-15 17:26:31 +0000

    Combine LoadManager back into Context

--- src2/context.rb
+++ src2/context.rb
@@ -43,7 +43,9 @@ class Context
     @warnings = []
     @log_msgs = JABA.running_tests? ? nil : [] # Disable logging when running tests
     @file_manager = FileManager.new
-    @load_manager = LoadManager.new(self, @file_manager)
+    @jdl_files = []
+    @jdl_includes = []
+    @jdl_file_lookup = {}
   end
 
   def input = @input
@@ -115,7 +117,7 @@ class Context
     JDL::TopLevelAPI.singleton.__internal_set_node(@top_level_node)
 
     init_root_paths
-    @load_manager.load_jaba_files
+    load_jaba_files
   end
 
   def init_root_paths
@@ -147,6 +149,88 @@ class Context
     log "src_root=#{src_root}"
     log "build_root=#{build_root}"
     log "temp_dir=#{temp_dir}"
+  end
+
+  def load_jaba_files
+    if src_root
+      process_load_path(src_root, fail_if_empty: true)
+    end
+
+    # Definitions can also be provided in a block form
+    #
+    Array(input.definitions).each do |block|
+      block_file = block.source_location[0].cleanpath
+      @jdl_files << block_file
+      execute_jdl(&block)
+    end
+
+    # Process include directives, accounting for included files including other files.
+    #
+    while !@jdl_includes.empty?
+      inc = @jdl_includes.pop
+      process_load_path(inc.path)
+    end
+  end
+
+  def process_load_path(p, fail_if_empty: false)
+    if !p.absolute_path?
+      JABA.error("'#{p}' must be an absolute path")
+    end
+
+    if !@file_manager.exist?(p)
+      JABA.error("'#{p}' does not exist", want_backtrace: false)
+    end
+
+    if @file_manager.directory?(p)
+      files = @file_manager.glob_files("#{p}/*.jaba")
+      if files.empty?
+        msg = "No .jaba files found in '#{p}'"
+        if fail_if_empty
+          JABA.error(msg, want_backtrace: false)
+        else
+          JABA.warn(msg)
+        end
+      else
+        files.each do |f|
+          process_jaba_file(f)
+        end
+      end
+    else
+      process_jaba_file(p)
+    end
+  end
+
+  def process_jaba_file(f)
+    if !f.absolute_path?
+      JABA.error("'#{f}' must be an absolute path")
+    end
+    f = f.cleanpath
+
+    if @jdl_file_lookup.has_key?(f)
+      # Already loaded. Ignore.
+      return
+    end
+    
+    @jdl_file_lookup[f] = nil
+    @jdl_files << f
+
+    execute_jdl(file: f)
+  end
+
+  IncludeInfo = Data.define(:path, :args)
+
+  def process_include(path)
+    if path.nil?
+      JABA.error("include requires a path")
+    end
+    if !path.absolute_path?
+      path = "#{$last_call_location.absolute_path.parent_path}/#{path}"
+    end
+    if path.wildcard?
+      @jdl_includes.concat(@file_manager.glob_files(path).map{|d| IncludeInfo.new(d, args)})
+    else
+      @jdl_includes << IncludeInfo.new(p ath, args)
+    end
   end
 
   def execute_jdl(*args, file: nil, str: nil, &block)
@@ -223,7 +307,7 @@ class Context
       callstack.slice!(jaba_run_idx..-1)
     end
 
-    candidates = @load_manager.jdl_files
+    candidates = @jdl_files
 
     # Extract any lines in the callstack that contain references to definition source files.
     #
