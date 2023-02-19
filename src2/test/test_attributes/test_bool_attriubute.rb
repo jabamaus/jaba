55593b586d7ab590979fd624a7e6d4f0b8e13536
jabamaus <49597106+jabamaus@users.noreply.github.com>
2023-02-19 13:45:15 +0000

    add commented out tests

--- src2/test/test_attributes/test_bool_attriubute.rb
+++ src2/test/test_attributes/test_bool_attriubute.rb
@@ -47,3 +47,58 @@ jtest 'only allows boolean values' do
     end
   end
 end
+
+=begin
+
+jtest 'works with required flag' do
+  assert_jaba_error "Error at #{src_loc('3C869B0D')}: 't.a' attribute requires a value. See #{src_loc('B959A565')}." do
+    jaba(barebones: true) do
+      type :test do
+        attr :a, type: :bool do # B959A565
+          flags :required
+        end
+      end
+      test :t do # 3C869B0D
+      end
+    end
+  end
+end
+
+jtest 'can be set from global_attrs' do
+  jaba(barebones: true, global_attrs: {
+    'a1': 'true',
+    'a2': false,
+    'a3': '1',
+    'a4': 0
+    }) do
+    open_type :globals do
+      attr :a1, type: :bool do
+        default false
+      end
+      attr :a2, type: :bool do
+        default true
+      end
+      attr :a3, type: :bool do
+        default false
+      end
+      attr :a4, type: :bool do
+        default true
+      end
+    end
+    type :test
+    test :t do
+      globals.a1.must_equal true
+      globals.a2.must_equal false
+      globals.a3.must_equal true
+      globals.a4.must_equal false
+    end
+  end
+
+  op = jaba(barebones: true, global_attrs: {'a': '10'}, want_exceptions: false) do
+    open_type :globals do
+      attr :a, type: :bool
+    end
+  end
+  op[:error].must_equal "'10' invalid value for ':a' attribute - [true|false|0|1] expected"
+end
+=end
