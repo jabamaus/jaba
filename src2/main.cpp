5e329864744c97443d985944f9e7f52e7313ed94
jabamaus <49597106+jabamaus@users.noreply.github.com>
2023-03-07 13:43:56 +0000

    update gems

--- src2/main.cpp
+++ src2/main.cpp
@@ -5,6 +5,8 @@
 #include "ireps.h"
 #include "mrubyjrf/mrb_state.h"
 
+extern "C" void mrb_mruby_dir_glob_gem_init(mrb_state * mrb);
+extern "C" void mrb_mruby_fileutils_gem_init(mrb_state*);
 extern "C" void mrb_mruby_onig_regexp_gem_init(mrb_state*);
 extern "C" void mrb_mruby_stringio_gem_init(mrb_state*);
 extern "C" const uint8_t jrf_core_ext_symbol[];
@@ -13,7 +15,9 @@ int run(int argc, char* argv[])
 {
   MrbState& mrb = MrbState::instance();
   mrb.open();
-  mrb_mruby_onig_regexp_gem_init(mrb.raw()); // TODO: make this nicer
+  mrb_mruby_dir_glob_gem_init(mrb.raw());
+  mrb_mruby_fileutils_gem_init(mrb.raw());
+  mrb_mruby_onig_regexp_gem_init(mrb.raw()); // TODO: move into mrubygems lib
   mrb_mruby_stringio_gem_init(mrb.raw());
   mrb.run(argc, argv, src2_jaba_symbol, jrf_core_ext_symbol);
   mrb.term();
