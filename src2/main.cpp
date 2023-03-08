42239a0a689521aeae1afb54abeebb93b4f1405a
jabamaus <49597106+jabamaus@users.noreply.github.com>
2023-03-08 13:26:19 +0000

    Use mrubygems_init/term

--- src2/main.cpp
+++ src2/main.cpp
@@ -4,22 +4,17 @@
 #include <stdexcept>
 #include "ireps.h"
 #include "mrubyjrf/mrb_state.h"
+#include "mrubygems.h"
 
-extern "C" void mrb_mruby_dir_glob_gem_init(mrb_state * mrb);
-extern "C" void mrb_mruby_fileutils_gem_init(mrb_state*);
-extern "C" void mrb_mruby_onig_regexp_gem_init(mrb_state*);
-extern "C" void mrb_mruby_stringio_gem_init(mrb_state*);
 extern "C" const uint8_t jrf_core_ext_symbol[];
 
 int run(int argc, char* argv[])
 {
   MrbState& mrb = MrbState::instance();
   mrb.open();
-  mrb_mruby_dir_glob_gem_init(mrb.raw());
-  mrb_mruby_fileutils_gem_init(mrb.raw());
-  mrb_mruby_onig_regexp_gem_init(mrb.raw()); // TODO: move into mrubygems lib
-  mrb_mruby_stringio_gem_init(mrb.raw());
+  mrubygems_init(mrb.raw());
   mrb.run(argc, argv, src2_jaba_symbol, jrf_core_ext_symbol);
+  mrubygems_term(mrb.raw());
   mrb.term();
   return EXIT_SUCCESS;
 }
