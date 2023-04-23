9a0b59cc8efd07e86f90ae909a441b4256a2324d
jabamaus <49597106+jabamaus@users.noreply.github.com>
2023-04-23 08:20:45 +0100

    Move error code into Context

--- src2/context.rb
+++ src2/context.rb
@@ -2,6 +2,14 @@ module JABA
   def self.set_context(c) = @@context = c
   def self.context = @@context
 
+  @@running_tests = false
+  def self.running_tests! = @@running_tests = true
+  def self.running_tests? = @@running_tests
+
+  def self.error(...) = JABA.context.error(...)
+  def self.warn(...) = JABA.context.warn(...)
+  def self.log(...) = JABA.context.log(...)
+
   class Context
     @@core_api_builder = nil
 
@@ -367,6 +375,20 @@ module JABA
         FileUtils.makedirs(log_fn.parent_path)
       end
       IO.write(log_fn, @log_msgs.join("\n"))
+    end
+
+    def error(msg, errobj: nil, want_err_line: true, want_backtrace: true, backtrace: nil)
+      msg = msg.ensure_end_with(".") if msg =~ /[a-zA-Z0-9']$/
+      e = JabaError.new(msg)
+      if errobj.proc?
+        backtrace = "#{errobj.source_location[0]}:#{errobj.source_location[1]}"
+        errobj = nil
+      end
+      bt = Array(errobj&.src_loc || backtrace || caller).map(&:to_s)
+      e.set_backtrace(bt)
+      e.instance_variable_set(:@want_backtrace, want_backtrace)
+      e.instance_variable_set(:@want_err_line, want_err_line)
+      raise e
     end
 
     def warn(msg, errobj: nil)
