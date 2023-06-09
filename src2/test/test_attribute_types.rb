b6b3f1c81ade8207c547b8b7e86c9019eedce831
jabamaus <49597106+jabamaus@users.noreply.github.com>
2023-06-09 22:23:31 +0100

    progress on tests

--- src2/test/test_attribute_types.rb
+++ src2/test/test_attribute_types.rb
@@ -1,6 +1,7 @@
 # TODO: should get this dynamically
+ATTR_VARIANTS = [:single, :array, :hash]
 ATTR_TYPES = {
-  basename: 'basename',
+  basename: "basename",
   bool: false,
   choice: :a,
   compound: nil,
@@ -14,39 +15,39 @@ ATTR_TYPES = {
   uuid: "uuid",
 }
 
+def each_attr
+  ATTR_VARIANTS.each do |av|
+    ATTR_TYPES.each do |at, default|
+      yield av, at, "(#{at} #{av})", default
+    end
+  end
+end
+
 jtest "fails if value not supplied when flagged with :required" do
   # Test required top level attr
-  ATTR_TYPES.keys.each do |at|
+  each_attr do |av, at, desc|
     jdl do
-      attr :top_single, type: at do
+      attr :a, variant: av, type: at do
         flags :required
-        items [:a, :b, :c] if at == :choice
-      end
-      attr :top_array, variant: :array, type: at do
-        flags :required
+        key_type :string if av == :hash
         items [:a, :b, :c] if at == :choice
       end
     end
-    assert_jaba_error "Error at #{src_loc("D43F2208")}: 'top_level' requires 'top_single' attribute to be set.", hint: at do
+    assert_jaba_error "Error at #{src_loc("D43F2208")}: 'top_level' requires 'a' attribute to be set.", hint: desc do
       jaba do end # D43F2208
     end
-    assert_jaba_file_error "'top_level' requires 'top_single' attribute to be set.", "8CF3DCA2", hint: at do
+    assert_jaba_file_error "'top_level' requires 'a' attribute to be set.", "8CF3DCA2", hint: desc do
       "# 8CF3DCA2"
-    end
-    assert_jaba_error "Error at #{src_loc("AE95398C")}: 'top_level' requires 'top_array' attribute to be set.", hint: at do
-      jaba do end # AE95398C
-    end
-    assert_jaba_file_error "'top_level' requires 'top_array' attribute to be set.", "9A8716BE", hint: at do
-      "# 9A8716BE"
     end
   end
 
   # Now test required node attr
-  ATTR_TYPES.keys.each do |at|
+  each_attr do |av, at, desc|
     jdl do
       node :node
-      attr "node/a", type: at do
+      attr "node/a", variant: av, type: at do
         flags :required
+        key_type :string if av == :hash
         items [:a, :b, :c] if at == :choice
       end
     end
@@ -63,31 +64,18 @@ jtest "fails if value not supplied when flagged with :required" do
 end
 =begin
 jtest "rejects modifying read only attributes" do
-  ATTR_TYPES.each do |at, default_|
+  each_attr do |av, at, desc, default_|
     jdl do
-      attr :single, type: at do
+      attr :a, variant: av, type: at do
         flags :read_only
         items [:a, :b, :c] if at == :choice
-        default default_ if !default_.nil?
-      end
-      if at == :compound
-      end
-      attr :array, variant: :array, type: at do
-        flags :read_only
-        items [:a, :b, :c] if at == :choice
-       # default [default_] if !default_.nil?
+        key_type :string if av == :hash
+        #default default_ 
       end
     end
     jaba do
-      JTest.assert_jaba_error "Error at #{JTest.src_loc("D4AE68B1")}: 'single' attribute is read only.", hint: at do
-        single default_ # D4AE68B1
-      end
-      JTest.assert_jaba_error "Error at #{JTest.src_loc("3C528822")}: 'array' attribute element is read only.", hint: at do
-        case at
-        when :compound
-        else
-          array default_ # 3C528822
-        end
+      JTest.assert_jaba_error "Error at #{JTest.src_loc("D4AE68B1")}: 'a' attribute is read only.", hint: desc do
+        a default_ # D4AE68B1
       end
     end
   end
