da8520480081e891ecdf5a2483650fbac3cfd383
jabamaus <49597106+jabamaus@users.noreply.github.com>
2024-10-30 07:09:40 +0000

    humble beginnings

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,56 @@
 #include "jrfcore/console_app.h"
 
-extern "C" const uint8_t jaba_irep[];
+struct Jaba : public ConsoleApp
+{
+  void main() override;
+};
+
+static mrb_value jdl_global_method(mrb_state*, mrb_value self)
+{
+  //app->mrb.args_begin();
+
+  //app->mrb.args_end();
+  return self;
+}
+
+static mrb_value jdl_method(mrb_state*, mrb_value self)
+{
+  //app->mrb.args_begin();
+
+  //app->mrb.args_end();
+  return self;
+}
+
+static mrb_value jdl_attr(mrb_state*, mrb_value self)
+{
+  //app->mrb.args_begin();
+
+  //app->mrb.args_end();
+  return self;
+}
+
+static mrb_value jdl_node(mrb_state*, mrb_value self)
+{
+  //app->mrb.args_begin();
+
+  //app->mrb.args_end();
+  return self;
+}
+
+void Jaba::main()
+{
+  RClass* rm = mrb.define_module("JDL");
+  mrb.define_module_method(rm, "global_method", jdl_global_method, MRB_ARGS_REQ(1));
+  mrb.define_module_method(rm, "method", jdl_method, MRB_ARGS_REQ(1));
+  mrb.define_module_method(rm, "attr", jdl_attr, MRB_ARGS_REQ(1));
+  mrb.define_module_method(rm, "node", jdl_node, MRB_ARGS_REQ(1));
+
+  IrepData* jdl_core = IRepRegistry::instance().lookup_irep("C:/james_projects/GitHub/jaba/src/jdl/core.rb");
+  mrb.load_irep(jdl_core);
+}
 
 int main(int argc, char* argv[])
 {
-  ConsoleApp app;
-  return app.run(argc, argv, jaba_irep);
-}
+  Jaba app;
+  return app.run(argc, argv);
+} 
