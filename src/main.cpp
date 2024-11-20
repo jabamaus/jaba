a66706c414223501d88f26eacbb0c9a898f8b818
jabamaus <49597106+jabamaus@users.noreply.github.com>
2024-11-20 20:06:46 +0000

    Update for new irep naming

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "jrfcore/console_app.h"
 
-extern "C" const uint8_t jaba_irep[];
+extern "C" const uint8_t jaba_jaba_irep[];
 
 int main(int argc, char* argv[])
 {
   ConsoleApp app;
-  return app.run(argc, argv, MRubyService::All, jaba_irep);
+  return app.run(argc, argv, MRubyService::All, jaba_jaba_irep);
 }
