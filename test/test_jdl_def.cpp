fa576b97021d500c089cca2c22f02e99d2c201a0
jabamaus <49597106+jabamaus@users.noreply.github.com>
2024-11-25 08:26:17 +0000

    Start testing jdlbuilder

--- test/test_jdl_def.cpp
+++ test/test_jdl_def.cpp
@@ -1,10 +1,32 @@
+#include <filesystem>
+#include <fstream>
 #include "test_common.h"
+#include "jdl_builder.h"
+
+bool test_jdl_str(JDLBuilder* b, const char* str)
+{
+  std::filesystem::create_directories(section_path);
+  section_path /= "test.jdl";
+  std::ofstream(section_path) << str;
+  bool result = load_jdl_file_dynamically(b, section_path.string());
+  section_path.remove_filename();
+  return result;
+}
 
 TEST_CASE("validates jdl path format")
 {
-  const char* jdl = R"(
-node :n
-attr "n|a" # 5CFB9574 pipes not allowed
+  JDLBuilder* b = init_jdl_builder();
+
+  WHEN("pipes used")
+  {
+    const char* jdl = R"(
+JDL.node :n
+JDL.attr "n|a"
 )";
-  REQUIRE(1 == 0);
+    REQUIRE(test_jdl_str(b, jdl) == false);
+    REQUIRE(jdl_errline(b) == 3);
+    REQUIRE(jdl_errmsg(b) == "'n|a' is in invalid format");
+    // TODO: check filename
+  }
+  term_jdl_builder(b);
 }
