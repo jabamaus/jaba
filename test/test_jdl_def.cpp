c813b882054fc1f6b2f7d46b66d1563b2fafa50d
jabamaus <49597106+jabamaus@users.noreply.github.com>
2024-11-26 14:36:23 +0000

    tests

--- test/test_jdl_def.cpp
+++ test/test_jdl_def.cpp
@@ -13,20 +13,40 @@ bool test_jdl_str(JDLBuilder* b, const char* str)
   return result;
 }
 
+void test_jdl_path(JDLBuilder* b, const char* jdl, int errline, const char* errmsg)
+{
+  REQUIRE(test_jdl_str(b, jdl) == false);
+  REQUIRE(jdl_errline(b) == errline);
+  REQUIRE(jdl_errmsg(b) == errmsg);
+}
+
 TEST_CASE("validates jdl path format")
 {
   JDLBuilder* b = init_jdl_builder();
 
-  WHEN("pipes used")
+  WHEN("pipes not slashes")
   {
-    const char* jdl = R"(
-JDL.node :n
-JDL.attr "n|a"
-)";
-    REQUIRE(test_jdl_str(b, jdl) == false);
-    REQUIRE(jdl_errline(b) == 3);
-    REQUIRE(jdl_errmsg(b) == "'n|a' is in invalid format");
-    // TODO: check filename
+    test_jdl_path(b, "JDL.node :n\nJDL.attr 'n|a'", 2, "'n|a' is in invalid format");
+  }
+  WHEN("double underscore")
+  {
+    test_jdl_path(b, "JDL.node :n\nJDL.attr 'n/a__b'", 2, "'n/a__b' is in invalid format");
+  }
+  WHEN("contains spaces")
+  {
+    test_jdl_path(b, "JDL.node :n\nJDL.attr 'n/a b'", 2, "'n/a b' is in invalid format");
+  }
+  WHEN("contains double slashes")
+  {
+    test_jdl_path(b, "JDL.node :n\nJDL.attr 'n//a_b'", 2, "'n//a_b' is in invalid format");
+  }
+  WHEN("starts with slash")
+  {
+    test_jdl_path(b, "JDL.node :n\nJDL.attr '/n/a'", 2, "'/n/a' is in invalid format");
+  }
+  WHEN("ends in slash")
+  {
+    test_jdl_path(b, "JDL.node :n\nJDL.attr 'n/a_b/'", 2, "'n/a_b/' is in invalid format");
   }
   term_jdl_builder(b);
 }
